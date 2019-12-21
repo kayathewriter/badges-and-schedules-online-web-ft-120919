0788f53c7537c87a68874aacc2c1aac984493990
Mikael Kaya <kayaswork@gmail.com>
2019-12-21 23:09:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,9 @@
-def badge_maker(names)
-  print "Hello, my name is #{names}"
+def badge_maker("Arel")
+  print "Hello, my name is #{name}"
 end
 
 def batch_badge_creator(names)
 names = ["Arel", "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def assign_rooms(1,2,3,4,5,6,7)
-  assign_rooms
+  
