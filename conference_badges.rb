a892ac4f5119fc903fe3d0bb9db03b23bdf39596
Mikael Kaya <kayaswork@gmail.com>
2019-12-21 22:56:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,4 +5,5 @@ end
 def batch_badge_creator(names)
 names = ["Arel", "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def assign_rooms
+def assign_rooms(1,2,3,4,5,6,7)
+  assign_rooms
